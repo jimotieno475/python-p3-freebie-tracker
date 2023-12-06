1722a94c5e1e2e447b96c06eab4bbe78a81ea43f
jim <jim.otieno@student.moringaschool.com>
2023-12-07 01:30:20 +0300

    completed

--- lib/models.py
+++ lib/models.py
@@ -22,6 +22,14 @@ class Company(Base):
     def __repr__(self):
         return f'<Company {self.name}>'
 
+    def give_freebie(self, dev, item_name, value):
+        new_freebie = Freeble(item_name=item_name, value=value, company=self, dev=dev)
+        return new_freebie
+
+    @classmethod
+    def oldest_company(cls):
+        return cls.query.order_by(cls.founding_year).first()
+
 class Dev(Base):
     __tablename__ = 'devs'
 
@@ -34,6 +42,18 @@ class Dev(Base):
     def __repr__(self):
         return f'<Dev {self.name}>'
     
+    def received_one(self, item_name):
+        for freebie in self.freebles:
+            if freebie.item_name == item_name:
+                return True
+        return False
+
+    def give_away(self, other_dev, freebie):
+        if freebie.dev == self:
+            freebie.dev = other_dev
+            return True
+        return False
+    
 class Freeble(Base):
     __tablename__='freebies'
 
@@ -43,3 +63,5 @@ class Freeble(Base):
     company_id = Column(Integer(), ForeignKey('companies.id'))
     dev_id = Column(Integer(), ForeignKey('devs.id'))
 
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
